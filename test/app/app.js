dec6f8efb07aaca9283744f197944f1608b2ff9b
bmc <brian.m.carlson@gmail.com>
2012-10-25 00:12:58 -0500

    add 'POST' support

--- test/app/app.js
+++ test/app/app.js
@@ -2,9 +2,17 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.json());
 
 app.get('/', function(req, res, next) {
   res.send('O hai!');
+});
+
+app.post('/data', function(req, res, next) {
+  if(req.body.name === 'brian') {
+    return res.send({user: 'brian'});
+  }
+  res.send(500, {pwnd: true});
 });
 
 module.exports = app;
--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
-var omf = require('./../');
+var assert = require('assert');
 
+var omf = require('./../');
 var app = require('./app/app');
 
 omf(app, function(app) {
@@ -18,6 +19,28 @@ omf(app, function(app) {
   app.get('/index.js', function(res) {
     res.has.statusCode(200);
     res.is.javascript();
+  });
+
+  app.get('/data', function(res) {
+    res.has.statusCode(404);
+  });
+
+  app.post('/data', {json: {name: 'brian'}}, function(res) {
+    res.has.statusCode(200);
+
+    it('returns valid json', function() {
+      assert.equal(this.response.body.user, 'brian');
+    });
+  });
+
+  describe('invalid post', function() {
+    app.post('/data', {json: {name: 'Aaron'}}, function(res) {
+      res.has.statusCode(500);
+
+      it('returns error json', function() {
+        assert.strictEqual(this.response.body.pwnd, true);
+      });
+    });
   });
 
 });
